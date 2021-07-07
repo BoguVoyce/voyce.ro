cf10ade26799dd673d82cb8bd0e45748ca18dfc9
bogvoicescu <bog.voicescu@gmail.com>
2021-07-07 17:46:43 +0300

    Recreated the portfolio with create-react-app, still in development

---
+++ src/components/project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Project = () => {
+    return (
+<div className="project">
+        <div className="project-about">
+        </div>
+        <img src="" alt="" className="project-photo" />
+        
+    </div>
+    )
+}
+
+export default Project;
