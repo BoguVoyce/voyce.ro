e20671e43514b8d61df1145b1628944a4ff89096
bogvoicescu <bog.voicescu@gmail.com>
2021-07-11 16:50:39 +0300

    Updated picture

--- src/components/about.js
+++ src/components/about.js
@@ -11,7 +11,7 @@ const About = () => {
                 <br />
                 <br /> - Feel free to take a look at my projects and please let me know what you think, even if you're not hiring.
             </h3>
-            <img src="https://scontent-otp1-1.xx.fbcdn.net/v/t1.6435-9/119434971_1867484343394323_3057137243549325249_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=SJXbM93k4LYAX8HRXPB&_nc_ht=scontent-otp1-1.xx&oh=d80478f29e7c23a75b6d516207b4fece&oe=60EA8121" alt="Me on a trip" className="about-photo" />
+            <img src="https://scontent-otp1-1.xx.fbcdn.net/v/t1.6435-9/119434971_1867484343394323_3057137243549325249_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=rEU-bCNtsNIAX8IanZt&_nc_ht=scontent-otp1-1.xx&oh=965ed834fc5e884fb32c0f7d36f6fb95&oe=60F06FE1" alt="Me on a trip" className="about-photo" />
         </div>
     )
 }
