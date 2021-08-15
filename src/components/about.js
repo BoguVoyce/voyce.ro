11d0c1eae664839c426da7e5af9b526aaa32c198
bogvoicescu <bog.voicescu@gmail.com>
2021-08-15 23:49:53 +0300

    changed the about section

--- src/components/about.js
+++ src/components/about.js
@@ -15,9 +15,11 @@ const About = () => {
                         <img className="card-img" src={myphoto} alt="Me on a trip outdoors" />
                     </div>
                     <div className="card-text">
-                        <p>- I have been studying Web Development for about 2 years now and I'm really passionate about my work. I like creating and styling websites, so far I've explored HTML, CSS, JavaScript, Bootstrap, React.js, npm and got some experience with Git, GitHub and VSCode. </p>
-                        <p>- I would love to work in web development and have the opportunity to learn more.</p>
-                        <p>- My studies have been slowed by my work which doesn't involve coding of any kind. Despite that, I continue to learn and look for a job that I can dedicate myself to.</p>
+                        <h3>Hello! I am Bogdan.</h3>
+                        <br />
+                        <p>I have been studying Web Development on my own for about 2 years now and I'm really passionate about technology;</p>
+                        <p>I like creating and managing websites, mainly Front-End;</p>
+                        <p>I work with HTML, CSS, JavaScript and have experience with Bootstrap, React.js, npm, Git, GitHub and VSCode.</p>
                     </div>
                 </div>
             </div>
